a16aed4ee678f66de5cb7aabc3598997a30110ea
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-03-01 00:34:37 +0200

    Начало ИИ

--- src/main/java/com/codenjoy/dojo/bomberman/client/YourSolver.java
+++ src/main/java/com/codenjoy/dojo/bomberman/client/YourSolver.java
@@ -40,16 +40,16 @@ public class YourSolver implements Solver<Board> {
 
     private Dice dice;
     private Board board;
-    Direction lastDirection = Direction.UP;
-    Direction newDirection = Direction.UP;
-    String returnDirectionString;
-    Collection<Point> destroyWalls;
-    Collection<Point> bombs;
-    Collection<Point> bombermans;
-    Collection<Point> meatChoppers;
-    Point myPosition;
-    Point goal;
-    boolean isGoalExist = true;
+    private Direction lastDirection = Direction.UP;
+    private Direction newDirection = Direction.UP;
+    private String returnDirectionString;
+    private Collection<Point> destroyWalls;
+    private Collection<Point> bombs;
+    private Collection<Point> bombermans;
+    private Collection<Point> meatChoppers;
+    private Point myPosition;
+    private Point goal;
+    private boolean isGoalExist = true;
 
     public YourSolver(Dice dice) {
         this.dice = dice;
@@ -109,29 +109,39 @@ public class YourSolver implements Solver<Board> {
         // Y 4   + 8 +   +
         // Y 5
 
-        // если цель выше ( getY() > моего)
+        // если цель выше ( getY() > моего.getY() )
         // то идем вверх при условии, что вверх-вниз можно ходить
         if((goal.getY() - myPosition.getY()) < 0){
             // если можем двигаться по вертикали
-            if(isMoveUpDown(myPosition)){
+            outDirectionString = step(board, myPosition, "UP");
 
-
-            // если можем двигаться по горизонтали
-            }else{
-
-            }
-
-        // движемся вниз при условии, что вверх-вниз можно ходить
+            // движемся вниз при условии, что вверх-вниз можно ходить
         }else if((goal.getY() - myPosition.getY()) > 0){
-
+            // если можем двигаться по вертикали
+            outDirectionString = step(board, myPosition, "DOWN");
 
         // находимся на одной линии Y
+        // (goal.getY() - myPosition.getY()) == 0
         }else{
-
-
+            // если можем двигаться по вертикали
+            if(myPosition.getX() % 2 == 1 ) {
+                outDirectionString = step(board, myPosition, "UP");
+            }else{
+                if((goal.getX() - myPosition.getX()) < 0){
+                    // идем влево
+                    outDirectionString = step(board, myPosition, "LEFT");
+                }else if((goal.getX() - myPosition.getX()) > 0){
+                    // идем вправо
+                    outDirectionString = step(board, myPosition, "RIGHT");
+                }else{
+                    // похоже сюда мы не попадем,
+                    // а если попадем, то значит, что мы родились взаперти
+                    outDirectionString = Direction.ACT.toString();
+                }
+            }
         }
 
-
+/*
         // если Х нечетная, то я могу двигаться только по вертикали
         if(isMoveUpDown(myPosition)){
             if(lastDirection.toString().equals("UP")){
@@ -195,9 +205,79 @@ public class YourSolver implements Solver<Board> {
                 outDirectionString = newDirection.toString();
             }
         }
+*/
 
         return outDirectionString;
     }
+
+    private Direction getDirection(String directionString){
+        Direction direction;
+        switch(directionString){
+            case "UP":
+                direction = Direction.UP;
+                break;
+            case "DOWN":
+                direction = Direction.DOWN;
+                break;
+            case "LEFT":
+                direction = Direction.LEFT;
+                break;
+            case "RIGHT":
+                direction = Direction.RIGHT;
+                break;
+            case "STOP":
+                direction = Direction.STOP;
+                break;
+            default:
+                direction = Direction.ACT;
+        }
+        return direction;
+    }
+
+    private Direction reverseDirection(String directionString){
+        Direction direction;
+        switch(directionString){
+            case "UP":
+                direction = Direction.DOWN;
+                break;
+            case "DOWN":
+                direction = Direction.UP;
+                break;
+            case "LEFT":
+                direction = Direction.RIGHT;
+                break;
+            case "RIGHT":
+                direction = Direction.LEFT;
+                break;
+            case "STOP":
+                direction = Direction.ACT;
+                break;
+            default:
+                direction = Direction.STOP;
+        }
+        return direction;
+    }
+
+    private String step(Board board, Point myPosition, String direction) {
+        String outDirectionString;
+        if(isPointEmpty(myPosition,direction.toUpperCase(), board)) {
+            newDirection = getDirection(direction.toUpperCase());
+            lastDirection = newDirection;
+            outDirectionString = newDirection.toString();
+        }else if(isPointEmpty(myPosition,reverseDirection(direction.toUpperCase()).toString(), board)){
+            newDirection = reverseDirection(direction.toUpperCase());
+            lastDirection = newDirection;
+            outDirectionString = newDirection.toString();
+
+        }else {
+            lastDirection = newDirection.inverted();
+            newDirection = Direction.ACT;
+            outDirectionString = newDirection.toString() + "," + lastDirection.toString();
+
+        }
+        return outDirectionString;
+    }
+
 
     public boolean isPointEmpty(Point point,String direction,Board board){
 
@@ -219,6 +299,7 @@ public class YourSolver implements Solver<Board> {
             case "RIGHT":
                 point.move(point.getX()+1,point.getY());
                 break;
+            default:
         }
         if(point.getX() <= 0
 //            || point.getX() >= sqrt(board.size())
